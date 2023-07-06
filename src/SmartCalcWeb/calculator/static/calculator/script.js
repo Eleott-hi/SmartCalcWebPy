909eea191ebc514a8f000d0807238c95a018fdb9
pintoved <pintoved@student.21-school.ru>
2023-07-06 23:23:01 +0700

    calculate func

--- src/SmartCalcWeb/calculator/static/calculator/script.js
+++ src/SmartCalcWeb/calculator/static/calculator/script.js
@@ -1,29 +1,33 @@
-var selectedInputId = 'result';
+var selectedInputId = "result";
 
 function appendToResult(value) {
   var selectedInput = document.getElementById(selectedInputId);
   selectedInput.value += value;
+  console.log(selectedInput.value);
 }
 
 function setInputId(inputId) {
-  var resultInput = document.getElementById('result');
-  var xValueInput = document.getElementById('xValue');
-  resultInput.removeAttribute('custom-focus');
-  xValueInput.removeAttribute('custom-focus');
+  var resultInput = document.getElementById("result");
+  var xValueInput = document.getElementById("xValue");
+  resultInput.removeAttribute("custom-focus");
+  xValueInput.removeAttribute("custom-focus");
 
   selectedInputId = inputId;
   var selectedInput = document.getElementById(selectedInputId);
-  selectedInput.setAttribute('custom-focus', '');
+  selectedInput.setAttribute("custom-focus", "");
 }
 
 function calculate() {
-  var input = document.getElementById('result').value;
-  var result = eval("Math.pow(" + input + ", 2)");
-  document.getElementById('result').value = result;
+  // var input = document.getElementById('result').value;
+  // input += "tnp"
+  // var result = eval("Math.pow(" + input + ", 2)");
+
+  sendPostRequest();
+  // document.getElementById('result').value = result;
 }
 
 function clearResult() {
-  document.getElementById(selectedInputId).value = '';
+  document.getElementById(selectedInputId).value = "";
 }
 
 function deleteLastElement() {
@@ -31,7 +35,6 @@ function deleteLastElement() {
   const currentInput = resultField.value;
   resultField.value = currentInput.slice(0, -1);
 }
-
 
 function animateCircle(button, x, y) {
   const rect = button.getBoundingClientRect();
@@ -54,100 +57,99 @@ function animateCircle(button, x, y) {
   }, 10);
 }
 
-
-window.addEventListener('keydown', function (event) {
+window.addEventListener("keydown", function (event) {
   var key = event.key;
   var button = null;
 
   switch (key) {
-    case '0':
+    case "0":
       button = document.querySelector('button[data-value="0"]');
       break;
-    case '1':
+    case "1":
       button = document.querySelector('button[data-value="1"]');
       break;
-    case '2':
+    case "2":
       button = document.querySelector('button[data-value="2"]');
       break;
-    case '3':
+    case "3":
       button = document.querySelector('button[data-value="3"]');
       break;
-    case '4':
+    case "4":
       button = document.querySelector('button[data-value="4"]');
       break;
-    case '5':
+    case "5":
       button = document.querySelector('button[data-value="5"]');
       break;
-    case '6':
+    case "6":
       button = document.querySelector('button[data-value="6"]');
       break;
-    case '7':
+    case "7":
       button = document.querySelector('button[data-value="7"]');
       break;
-    case '8':
+    case "8":
       button = document.querySelector('button[data-value="8"]');
       break;
-    case '9':
+    case "9":
       button = document.querySelector('button[data-value="9"]');
       break;
-    case '+':
+    case "+":
       button = document.querySelector('button[data-value="+"]');
       break;
-    case '-':
+    case "-":
       button = document.querySelector('button[data-value="-"]');
       break;
-    case '*':
+    case "*":
       button = document.querySelector('button[data-value="*"]');
       break;
-    case '/':
+    case "/":
       button = document.querySelector('button[data-value="/"]');
       break;
-    case '.':
+    case ".":
       button = document.querySelector('button[data-value="."]');
       break;
-    case '%':
+    case "%":
       button = document.querySelector('button[data-value="%"]');
       break;
-    case '(':
+    case "(":
       button = document.querySelector('button[data-value="("]');
       break;
-    case ')':
+    case ")":
       button = document.querySelector('button[data-value=")"]');
       break;
-    case '=':
+    case "=":
       button = document.querySelector('button[data-value="="]');
       break;
-    case 'Escape':
+    case "Escape":
       button = document.querySelector('button[data-value="C"]');
       break;
-    case 'a':
+    case "a":
       button = document.querySelector('button[data-value="Math.acos("]');
       break;
-    case 'c':
+    case "c":
       button = document.querySelector('button[data-value="Math.cos("]');
       break;
-    case 's':
+    case "s":
       button = document.querySelector('button[data-value="Math.asin("]');
       break;
-    case 'i':
+    case "i":
       button = document.querySelector('button[data-value="Math.sin("]');
       break;
-    case 't':
+    case "t":
       button = document.querySelector('button[data-value="Math.atan("]');
       break;
-    case 'n':
+    case "n":
       button = document.querySelector('button[data-value="Math.tan("]');
       break;
-    case 'r':
+    case "r":
       button = document.querySelector('button[data-value="Math.sqrt("]');
       break;
-    case '^':
+    case "^":
       button = document.querySelector('button[data-value="Math.pow("]');
       break;
-    case 'Backspace':
+    case "Backspace":
       button = document.querySelector('button[data-value="Backspace"]');
       break;
-    case 'x':
+    case "x":
       button = document.querySelector('button[data-value="x"]');
       break;
   }
@@ -158,4 +160,76 @@ window.addEventListener('keydown', function (event) {
   }
 });
 
+function getCsrfToken() {
+  const csrfCookie = document.cookie.match(/csrftoken=([\w-]+)/);
+  return csrfCookie ? csrfCookie[1] : "";
+}
 
+function sendPostRequest() {
+  const url = "/calculate";
+
+  const expression = document.getElementById("result").value;
+
+  const payload = JSON.stringify({
+    expression: expression,
+  });
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": getCsrfToken(),
+    },
+    body: payload,
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return {
+          result: "Invalid Expression",
+        };
+      }
+    })
+    .then((data) => {
+      document.getElementById("result").value = data.result;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+
+function sendPostRequest() {
+  const url = "/calculate";
+
+  const expression = document.getElementById("result").value;
+
+  const payload = JSON.stringify({
+    expression: expression,
+  });
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": getCsrfToken(),
+    },
+    body: payload,
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return {
+          result: "Invalid Expression",
+        };
+      }
+    })
+    .then((data) => {
+      document.getElementById("result").value = data.result;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
