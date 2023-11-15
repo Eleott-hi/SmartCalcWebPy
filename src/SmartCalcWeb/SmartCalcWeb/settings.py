51685714be426ec7779d1594c5209e123f6a660c
pintoved <laptev18vv@gmail.com>
2023-11-15 14:10:52 +0700

    nvm packege update, nvm 16,  cors allowed origins update

--- src/SmartCalcWeb/SmartCalcWeb/settings.py
+++ src/SmartCalcWeb/SmartCalcWeb/settings.py
@@ -154,7 +154,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5173",
+]
 
 class RotatingFilename(TimedRotatingFileHandler):
     def doRollover(self):
